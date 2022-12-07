b628db12335134cf7aeb5343774432706828d314
kacper mucha <kacpermucha420@gmail.com>
2022-12-07 18:02:25 +0100

    .

---
+++ Zajecia 5/main.js
@@ -0,0 +1,82 @@
+class FuncLibrary{
+    constructor(array){
+        if(array != null){
+            this.container = array;
+        }else{
+            this.container = [];
+        }
+        this.subscribers = [];
+    }
+
+    subscribe(subscriber){
+        this.subscribers.push(subscriber);
+    }
+
+    notify(){
+        this.subscribers.forEach(element => {
+            element.update(this.container);
+        });
+    }
+
+    addToLibrary(callback){
+        this.container.push(callback);
+        this.notify();
+    }
+
+}
+
+class Subscriber{
+
+    constructor(){
+        this.functions = [];        
+    }
+
+    update(funcArray){
+        funcArray.forEach(element => {
+            this.functions.push(element);
+        })
+    }
+
+}
+
+let publisher = new FuncLibrary();
+let subscriber = new Subscriber();
+publisher.subscribe(subscriber);
+publisher.addToLibrary(saveCToSessionStorage);
+publisher.addToLibrary(discoverPowerBallNumber);
+
+function interval(){
+    let timer = 1;
+    let arr = subscriber.functions;
+    setInterval(
+        () => {
+        arr.forEach(element => {
+            element(timer);
+        });
+        timer++;
+        }
+        , 2000);
+}
+
+interval();
+
+
+class Logger {
+  static log(data) {
+    console.log(data)
+  }
+
+  
+
+}
+
+function saveCToSessionStorage(data) {
+  console.log('[reader C]', data)
+  const storageData = { data }
+  sessionStorage.setItem('C', JSON.stringify(storageData))
+}
+
+function discoverPowerBallNumber(data) {
+  const number = Math.floor(Math.random() * data * 100)
+  console.log('[powerball number]', number)
+}
