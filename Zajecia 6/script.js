1fff685fd37a0de62ca8dffd860916b510b2bc6c
kacper mucha <kacpermucha420@gmail.com>
2023-01-18 23:40:40 +0100

    pilka w dziurce

--- Zajecia 6/script.js
+++ Zajecia 6/script.js
@@ -1,46 +1,134 @@
-      var ball = document.getElementById("ball");
-      var hole = document.getElementById("hole");
-      var ballX = 0;
-      var ballY = 0;
-      var holeX = window.innerWidth / 2 - 50;
-      var holeY = window.innerHeight / 2 - 50;
-      var score = 0;
-      var startTime;
-      function moveBall(event) {
-        ballX = event.clientX - 25;
-        ballY = event.clientY - 25;
-        ball.style.left = ballX + "px";
-        ball.style.top = ballY + "px";
+const gameBoard = document.querySelector(".game_board");
+let hole;
+let timer = document.getElementById("timer");
+let ball = document.querySelector("#ball");
+let container = document.getElementsByClassName("container")[0];
+let instruction = document.getElementById("instructionForm");
+let btnStart = document.getElementById("start");
+let score1 = document.getElementById("score");
+let score = 0;
+let ballY, ballX;
+let speedX = 0,
+  speedY = 0;
+let posX = 1000,
+  posY = 500;
+let m = 0;
+let s = -1;
+let highScore = 0;
+let highScore1 = document.querySelector("#highscore")
+let minW = 40;
+let maxW = minW + parseInt(gameBoard.offsetWidth) - 85;
+let minH = 150;
+let maxH = minH + parseInt(gameBoard.offsetHeight) - 85;
 
-        if (Math.abs(ballX - holeX) < 50 && Math.abs(ballY - holeY) < 50) {
-          score++;
-          ballX = 0;
-          ballY = 0;
-          ball.style.left = ballX + "px";
-          ball.style.top = ballY + "px";
-        }
-      }
+const maxY = gameBoard.clientWidth - ball.clientWidth;
+const maxX = gameBoard.clientHeight - ball.clientHeight;
 
-      function startTimer() {
-        startTime = Date.now();
-        setTimeout(endTimer, 60000);
-      }
+console.log(typeof(gameBoard.offsetHeight));
+console.log(typeof(gameBoard.offsetWidth));
 
-      function endTimer() {
-        var endTime = Date.now();
-        var timeElapsed = endTime - startTime;
-        console.log("Czas: " + (timeElapsed / 1000) + " sekund");
-        console.log("Liczba trafień: " + score);
+window.addEventListener("deviceorientation", moveBall)
 
-      var record = {
-time: timeElapsed,
-score: score
-};
-      var recordsList = JSON.parse(localStorage.getItem("records")) || [];
-      recordsList.push(record);
-      localStorage.setItem("records", JSON.stringify(recordsList));
-      console.log("Lista rekordów: ", recordsList);
-      }
-      
-      window.addEventListener("devicemotion", moveBall);
-      window.addEventListener("click", startTimer);
+function start() {
+  putHole();
+  console.log(posX, posY, ballX, ballY);
+  instruction.style.visibility = "hidden";
+  ball.style.visibility = "visible";
+
+  counter();
+  setInterval(() => {
+    counter();
+  }, 1000);
+}
+
+btnStart.addEventListener("click", start);
+
+function counter() {
+  s++;
+
+  if (s == 60) {
+    s = 0;
+    m++;
+  }
+
+  if (m == 1){
+    collisionDone();
+    m = 0;
+  }
+  displayTime();
+}
+
+function displayTime() {
+  let time;
+  if (s < 10) {
+    displayS = "0" + s;
+  } else displayS = s;
+  if (m < 10) {
+    displayM = "0" + m;
+  } else displayM = m;
+  time = displayM + ":" + displayS;
+  timer.innerHTML = time;
+}
+
+function moveBall(e) {
+  let x = e.beta;
+  let y = e.gamma;
+
+  if (x > 90) x = 90;
+  if (x < -90) x = -90;
+
+  x += 90;
+  y += 90;
+
+  ball.style.top = `${(maxY * y) / 180}px`;
+  ball.style.left = `${(maxX * x) / 180}px`;
+  checkCollision(ball, hole)
+}
+
+function putHole() {
+  hole = document.createElement("div");
+  hole.classList.add("hole");
+  hole.style.visibility = "visible";
+
+  let rndW = Math.random()* (gameBoard.clientWidth - 75)
+  let rndH = Math.random()* (gameBoard.clientHeight - 75)
+  console.log(rndH)
+  console.log(rndW)
+
+  hole.style.left = rndW + "px";
+  hole.style.top = rndH + "px";
+  container.appendChild(hole);
+}
+
+function checkCollision(ball, hole){
+  const ballX = parseInt(ball.style.left)
+  const ballY = parseInt(ball.style.top)
+
+  const holeX = parseInt(hole.style.left)
+  const holeY = parseInt(hole.style.top)
+
+  if (
+    ballY < holeY + 20 &&
+    ballY > holeY - 20 &&
+    ballX < holeX + 20 &&
+    ballX > holeX - 20
+  ){
+    score++
+    score1.textContent=`${score}`
+
+    hole.remove()
+    putHole()
+  }
+}
+
+function collisionDone() {
+    let info = `End game! Your score is: ${score}`;
+    if(score >= highScore){
+      highScore = score 
+      highScore1.textContent = highScore;
+    }
+    score = 0;
+    score1.textContent = score;
+    
+    alert(info);
+}
